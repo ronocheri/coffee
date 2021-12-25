576b75cbeb4c990f92a586141df1f7fbd8f4bf3e
ronocheri <ronocheri@gmail.com>
2021-12-25 11:45:54 +0200

    coffe project

---
+++ src/components/ArticlePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+
+function ArticlePage()
+{
+    return(
+        <Paper>
+          <h1>
+            Article
+          </h1>
+          <p>Here is some text</p>
+        </Paper>
+      );
+}
+export default ArticlePage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import ArticlePage from "./ArticlePage";
+
+function HomePage()
+{
+    return (
+        <Paper>
+          <img
+            src="https://www.rlogical.com/wp-content/uploads/2018/09/blog7.png"
+          ></img>
+          <h1>
+            Welcome!
+          </h1>
+        </Paper>
+      );
+}
+export default HomePage;
